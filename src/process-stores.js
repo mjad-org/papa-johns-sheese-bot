ce923f7ffbd4ea46df642f472518f285c3bad138
mjad <mdalek@gmail.com>
2019-02-18 23:55:51 +0000

    refactored to a single module, check before downloading store json, reduce console output to summary message

--- src/process-stores.js
+++ src/process-stores.js
@@ -2,7 +2,7 @@ const fsp = require('fs-extra');
 const storeJsonPath = './stores/';
 const {HashMap} = require('hashmap');
 const superagent = require('superagent');
-
+const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
 
 exports.processStores = async () => {
 	/**
@@ -14,7 +14,7 @@ exports.processStores = async () => {
 		try {
 			const names = await fsp.readdir(storeJsonPath);
 			for (let i = 0; i < names.length; i++) {
-				console.log(localPath + names[i]);
+				console.log(storeJsonPath + names[i]);
 				let dat = await fsp.readJSON(storeJsonPath + names[i]);
 				dat.map((x) => {
 					retStores.set(x.value, x.label);
@@ -36,12 +36,13 @@ exports.processStores = async () => {
 		const storeUrl = 'https://www.papajohns.co.uk/stores/' + locationUrl + '/pizzas.aspx';
 		try {
 			const response = await superagent.get(storeUrl);
-			if (response.statusType == response.ok) {
-				console.log('Checking Url ' + storeUrl);
+			// 2 == OK
+			if (response.statusType == 2) {
 				if (response.text.indexOf('Sheese') !== -1) {
 					return true;
 				}
 			} else {
+				console.error(response);
 				throw new Error('Bad Response');
 			}
 		} catch (e) {
@@ -50,23 +51,68 @@ exports.processStores = async () => {
 		return false;
 	}
 
-	const storesMap = await readFiles();
-	const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
-
-	// run checkStore sequentially & slowly to avoid ddos!!
-	for (let s of storesMap.entries()) {
-		let storeKey = s[0];
-		let storeLabel = s[1];
-
-		await waitFor(1000);
-		checkStore(storeKey).then((status) => {
-			if (status) {
-				console.log(storeLabel + ' Has Vegan Sheese Pizzas !!!!!');
-			} else {
-				console.log(storeLabel + ' Has No Vegan Sheese Pizzas');
-			}
+	/**
+	 * Queries json store service
+	 */
+	async function getStoresJsonWriteToFiles() {
+		const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+		const storeSvc = 'https://www.papajohns.co.uk/services/storenamesearch.aspx?term=';
+		const localPath = './stores/';
+		const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
+		alphabet.forEach(async (letter) => {
+			await waitFor(2000);
+			let storeFileName = letter + '-stores.json';
+			console.log(storeFileName);
+			let file = fs.createWriteStream(localPath + storeFileName);
+			https.get(storeSvc + letter, (res) => {
+				console.log(res.statusCode);
+				res.pipe(file);
+			});
 		});
+	}
+
+	/**
+	 * Queries json store service
+	 * @param {HashMap} storesMap hashmap of stores
+	 */
+	async function checkAllStores(storesMap) {
+		const yesStores = new HashMap();
+		const noStores = new HashMap();
+
+		// run checkStore sequentially & slowly to avoid ddos!!
+		for (let s of storesMap.entries()) {
+			let storeKey = s[0];
+			let storeLabel = s[1];
+
+			await waitFor(300);
+			checkStore(storeKey).then((status) => {
+				if (status) {
+					yesStores.set(storeKey, storeLabel);
+				} else {
+					noStores.set(storeKey, storeLabel);
+				}
+			});
+		}
+
+		console.log(yesStores.size + ' Stores have vegan sheese pizzas');
+		console.log(noStores.size + ' Stores do not have vegan sheese pizzas');
+	}
+
+	// start
+
+	try {
+		if (fsp.existsSync(storeJsonPath)) {
+			if (fsp.readdirSync(storeJsonPath).length < 1) {
+				getStoresJsonWriteToFiles();
+			}
+		}
+		let s = await readFiles();
+		checkAllStores(s);
+	} catch (e) {
+		console.error(e);
 	}
 
 	return 'done';
 };
+
+
--- src/run.js
+++ src/run.js
@@ -1,5 +1,4 @@
 const proc = require('./process-stores');
-// const getStoreList = require('./get-store-list');
 
 (async () => {
 	proc.processStores();

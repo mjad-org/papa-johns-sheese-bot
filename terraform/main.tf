1f1cef5a4b800a0829ce7bc08fa00ef70f5e5253
Matt D'Albert <mdalek@gmail.com>
2022-06-23 17:29:03 +0100

    Switch to public project v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,20 +3,20 @@ provider "azuredevops" {
   org_service_url       = var.ado_service_url
 }
 
-resource "azuredevops_serviceendpoint_github" "serviceendpoint_github" {
-  project_id            = azuredevops_project.my_project.id
-  service_endpoint_name = "GithHub Personal Access Token"
-}
-
-# resource "azuredevops_git_repository" "imported_repo" {
-#   project_id = azuredevops_project.my_project.id
-#   name       = "Imported Repo"
-#   initialization {
-#     init_type   = "Import"
-#     source_type = "Git"
-#     source_url  = var.github_repo_source_url
-#   }
+# resource "azuredevops_serviceendpoint_github" "serviceendpoint_github" {
+#   project_id            = azuredevops_project.my_project.id
+#   service_endpoint_name = "GithHub Personal Access Token"
 # }
+
+resource "azuredevops_git_repository" "imported_repo" {
+  project_id = azuredevops_project.my_project.id
+  name       = "Imported Repo"
+  initialization {
+    init_type   = "Import"
+    source_type = "Git"
+    source_url  = var.github_repo_source_url
+  }
+}
 
 resource "azuredevops_project" "my_project" {
   name               = var.project_name
